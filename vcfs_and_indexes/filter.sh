398b660bd4f7a28c9a969eed28c5eebb19de3e4f
Adewale Joseph OGUNLEYE <30349247+josoga2@users.noreply.github.com>
2022-05-20 15:16:09 +0200

    HackBio_Public_Genomics_Dataset

---
+++ vcfs_and_indexes/filter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+bcftools view --force-samples -S bangladesh_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > bangladesh.vcf
+bcftools view --force-samples -S china_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > china.vcf
+bcftools view --force-samples -S colombia 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > colombia.vcf
+bcftools view --force-samples -S finland_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > finland.vcf
+bcftools view --force-samples -S gambia_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > gambia.vcf
+bcftools view --force-samples -S greece_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > greece.vcf
+bcftools view --force-samples -S india_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > india.vcf
+bcftools view --force-samples -S italy_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > italy.vcf
+bcftools view --force-samples -S japan_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > japan.vcf
+bcftools view --force-samples -S kenya_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > kenya.vcf
+bcftools view --force-samples -S nigeria_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > nigeria.vcf
+bcftools view --force-samples -S pakistan_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > pakistan.vcf
+bcftools view --force-samples -S peru_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > peru.vcf
+bcftools view --force-samples -S puerto_rico 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > puerto_rico.vcf
+bcftools view --force-samples -S sierra_leone 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > sierra_leone.vcf
+bcftools view --force-samples -S sri_lanka 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > sri_lanka.vcf
+bcftools view --force-samples -S uk_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > uk.vcf
+bcftools view --force-samples -S vietnam_id 20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chrY.recalibrated_variants.vcf > vietnam.vcf
+
